ece6f7ea2ffaa129bb365bdcbc8a99e7bbc5d298
Ethan C <eymchong@gmail.com>
2025-03-15 13:10:07 +1100

    finally made ocr work

--- OCR.py
+++ OCR.py
@@ -1,8 +1,10 @@
 import pytesseract
 from PIL import Image
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
 
 # Load an image
-img = Image.open('images/image1.png')
+img = Image.open('images/image1.jpg')
 
 # Perform OCR
 text = pytesseract.image_to_string(img)
